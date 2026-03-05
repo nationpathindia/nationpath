10e4f6d8c44b5163df7a8c97d3eb6bc2ab49767a
Nationpath <nationpathindia@gmail.com>
2026-03-05 22:38:54 +0530

    search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
-
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export default async function SearchPage({
   searchParams,
 }: {
